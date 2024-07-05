a0b38f506aabd8cd38bd54c45b3fe1e5ac7a2522
rishita <rgrover_be21@thapar.edu>
2024-07-06 00:10:07 +0530

    bcrypt

--- backend/models/student.js
+++ backend/models/student.js
@@ -1,4 +1,5 @@
 const mongoose=require('mongoose');
+const bcrypt=require('bcrypt');
 const studentSchema=new mongoose.Schema({
     name: {
         type: String,
@@ -28,6 +29,22 @@ const studentSchema=new mongoose.Schema({
         minlength:8
     }
 })
+studentSchema.pre('save',async function(next){
+    const student=this;
+    if(!student.isModified('password')) return next();
+    try{
+        // hash password generation
+        const salt = await bcrypt.genSalt(10);
 
+        // hash password
+        const hashedPassword = await bcrypt.hash(student.password, salt);
+        
+        // Override the plain password with the hashed one
+        student.password = hashedPassword;
+        next();
+    }catch(err){
+        return next(err);
+    }
+})
 const student=mongoose.model('student',studentSchema)
 module.exports=student
